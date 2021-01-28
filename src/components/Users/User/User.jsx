157e1205e69695057c40445669779dab22519686
alekseyvy <alekseyvy@gmail.com>
2021-01-29 02:25:59 +0300

    feature: lession 52

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -6,7 +6,7 @@ const User = ({user, follow}) => {
   return (
     <div className={styles.container}>
       <div className={styles.avatar_container}>
-        <img className={styles.avatar} src={avatar} alt={'Avatar'}/>
+        <img className={styles.avatar} src={user.photos.small || avatar} alt={'Avatar'}/>
         <button onClick={() => follow(user.id)}>{user.followed === true ? 'unfollow' : 'follow'}</button>
       </div>
       <div className={styles.user_info_container}>
@@ -15,7 +15,7 @@ const User = ({user, follow}) => {
             <strong>Full Name:</strong> {user.name}
           </div>
           <div>
-            <strong>Status:</strong> {user.status || "No status!"}
+            <strong>Status:</strong> {user.status || "No status"}
           </div>
         </div>
       </div>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,16 +4,19 @@ import axios from "axios";
 
 const Users = (props) => {
 
-  if(props.users.length === 0){
+  const getUsers = () => {
     axios.get('https://social-network.samuraijs.com/api/1.0/users').then((resp) => {
       props.setUsers(resp.data.items)
     })
   }
 
+
   return (
     <div>
       {
-        props.users.map((user) => {
+        props.users.length === 0
+          ? <button onClick={getUsers}>Get Users</button>
+          : props.users.map((user) => {
           return (
             <User key={user.id} user={user} follow={props.follow}/>
           )
